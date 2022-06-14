1929b77f62c9aadaa6553db643e6bd8784f0eca0
Ryan <fertigryan66@gmail.com>
2022-06-13 21:11:13 -0500

    updating

--- static/js/app.js
+++ static/js/app.js
@@ -28,3 +28,29 @@ function buildTable(data) {
       );
     });
   }
+
+// creating a function to 'listen/react' to user input
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+
+  // listen for the event
+d3.selectAll("#filter-btn").on('click', handleClick);
+
+//building final table
+buildTable(tableData);
